5fa9ff44676aba2ed1d1afbe86bf2cdef31ace03
zarim1317 <114936829+zarim1317@users.noreply.github.com>
2022-10-05 13:23:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+(function ($) {
+  "use strict";
+
+  new WOW().init();
+
+
+
+  // background image
+  function dataBackgroundImage() {
+    $("[data-bgimg]").each(function () {
+      var bgImgUrl = $(this).data("bgimg");
+      $(this).css({
+        "background-image": "url(" + bgImgUrl + ")", // concatenation
+      });
+    });
+  }
+
+  $(window).on("load", function () {
+    dataBackgroundImage();
+  });
+
+  //for carousel slider of the slider section
+  $(".slider_area").owlCarousel({
+    animateOut: "fadeOut",
+    autoplay: true,
+    loop: true,
+    nav: false,
+    autoplayTimeout: 6000,
+    items: 1,
+    dots: true,
+  });
+
+  //tooltip active
+  $(".action_links ul li a, .quick_button a").tooltip({
+    animated: "fade",
+    placement: "top",
+    container: "body",
+  });
+
+
+  
+
+  
+  // blog section
+  $(".blog_column3").owlCarousel({
+    autoplay: true,
+    loop: true,
+    nav: true,
+    autoplayTimeout: 5000,
+    items: 3,
+    dots: false,
+    margin: 30,
+    navText: [
+      '<i class="ion-chevron-left"></i>',
+      '<i class="ion-chevron-right"></i>',
+    ],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  });
+
+})(jQuery);
